9a1ec25f648c41f2e7551b68c9b6560f6e134252
“Sumit <er.sumitarora@gmail.com>
2015-02-26 23:39:04 -0500

    adding hadoop distribution

--- mc-hadoop/provisioners/setup.sh
+++ mc-hadoop/provisioners/setup.sh
@@ -6,3 +6,8 @@ sudo apt-get -y install default-jdk
 
 sudo apt-get -y install ssh
 sudo apt-get -y install rsync 
+
+sudo wget http://apache.parentingamerica.com/hadoop/common/hadoop-2.6.0/hadoop-2.6.0.tar.gz
+sudo mv hadoop-2.6.0.tar.gz /usr/local/
+
+sudo tar -xvf hadoop-2.6.0.tar.gz
